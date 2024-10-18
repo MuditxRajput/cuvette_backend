527332142ca7bf734f0a1d32c4152046fff00dac
Mudit Rajput <muditrajputpersonal@gmail.com>
2024-10-19 01:44:59 +0530

    'db'

--- src/db/connectionDB.js
+++ src/db/connectionDB.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 const connectionDB =async()=>{
     try {
-       const connection = await mongoose.connect(process.env.MONGO_URL)
+       const connection = await mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 20000,})
        console.log("Database is connected",connection.connection.host);
         
     } catch (error) {
